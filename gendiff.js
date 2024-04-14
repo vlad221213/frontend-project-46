ce4645bca4055f5e3f1361ee4af55a3699bca00e
vlad221213 <vlad221213836@gmail.com>
2024-04-14 17:31:53 +0700

    add format optin & filepath arguments

--- gendiff.js
+++ gendiff.js
@@ -5,6 +5,8 @@ const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath1>')
+  .option('-f, --format [type]', 'output format')
   .option('-V, --version', 'output the version number');
 
 program.parse();
