c29f554c7f6ec1cd68af00c97b9802f3964455b2
vlad221213 <vlad221213836@gmail.com>
2024-04-22 17:43:20 +0700

    refactoring

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import parse from './utils.js';
 const genDiff = (filePath1, filePath2) => {
   const file1 = parse(readFileSync(filePath1));
   const file2 = parse(readFileSync(filePath2));
-  const file1Collback = (acc, [key, value]) => {
+  const file1Array = Object.entries(file1);
+  const itemsArray1 = file1Array.reduce((acc, [key, value]) => {
     if (!Object.hasOwn(file2, key)) {
       acc.push(`- ${key}: ${value}`);
     }
@@ -18,19 +19,15 @@ const genDiff = (filePath1, filePath2) => {
       acc.push(`+ ${key}: ${file2[key]}`);
     }
     return acc;
-  };
-  const file1Array = Object.entries(file1);
-  const itemsArray1 = file1Array.reduce(file1Collback, []);
-  const file2Collback = (acc, [key, value]) => {
+  }, []);
+  const file2Array = Object.entries(file2);
+  const itemsArray2 = file2Array.reduce((acc, [key, value]) => {
     if (!Object.hasOwn(file1, key)) {
       acc.push(`+ ${key}: ${value}`);
     }
     return acc;
-  };
-  const file2Array = Object.entries(file2);
-  const itemsArray2 = file2Array.reduce(file2Collback, []);
+  }, []);
   const unionItemsArray = [...itemsArray1, ...itemsArray2].sort((a, b) => a[2].localeCompare(b[2]));
-  const result = `{\n${unionItemsArray.join('\n')}\n}`;
-  return result;
+  return `{\n${unionItemsArray.join('\n')}\n}`;
 };
 export default genDiff;
