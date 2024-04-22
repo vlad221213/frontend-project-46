4654fe1c06499a4a1e99651bb7c60f7b5eab27ab
vlad221213 <vlad221213836@gmail.com>
2024-04-22 16:53:07 +0700

    add files comparison

--- src/index.js
+++ src/index.js
@@ -1,10 +1,41 @@
+/* eslint-disable no-param-reassign */
 /* eslint-disable import/extensions */
 import { readFileSync } from 'node:fs';
 import parse from './utils.js';
 
 const genDiff = (filePath1, filePath2) => {
-  const file1 = readFileSync(filePath1);
-  const file2 = readFileSync(filePath2);
-  return parse(file1);
+  const file1 = parse(readFileSync(filePath1));
+  const file2 = parse(readFileSync(filePath2));
+  const itemsArray1 = Object.entries(file1).reduce((acc, [key, value]) => {
+    if (!Object.hasOwn(file2, key)) {
+      acc.push(`- ${key}: ${value}`);
+    }
+    if (Object.hasOwn(file2, key) && file1[key] === file2[key]) {
+      acc.push(`  ${key}: ${value}`);
+    }
+    if (Object.hasOwn(file2, key) && file1[key] !== file2[key]) {
+      acc.push(`- ${key}: ${file1[key]}`);
+      acc.push(`+ ${key}: ${file2[key]}`);
+    }
+    return acc;
+  }, []);
+  const itemsArray2 = Object.entries(file2).reduce((acc, [key, value]) => {
+    if (!Object.hasOwn(file1, key)) {
+      acc.push(`+ ${key}: ${value}`);
+    }
+    return acc;
+  }, []);
+  const unionItemsArray = [...itemsArray1, ...itemsArray2];
+  unionItemsArray.sort((a, b) => {
+    if (a[2] < b[2]) {
+      return -1;
+    }
+    if (a[2] > b[2]) {
+      return 1;
+    }
+    return 0;
+  });
+  const result = `{\n${unionItemsArray.join('\n')}\n}`;
+  return result;
 };
 export default genDiff;
