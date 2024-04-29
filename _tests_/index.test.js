51c085828433a9a46e82b79404831223042b5dd2
vlad221213 <vlad221213836@gmail.com>
2024-04-29 17:39:06 +0700

    add YMLtest

--- _tests_/index.test.js
+++ _tests_/index.test.js
@@ -3,6 +3,9 @@
 import genDiff from '../src/index.js';
 
 const outPut = '{\n- follow: false\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ timeout: 20\n+ verbose: true\n}';
-test('genDiff', () => {
+test('JSONtest', () => {
   expect(genDiff('_fixtures_/file1.json', '_fixtures_/file2.json')).toEqual(outPut);
+});
+test('YMLtest', () => {
+  expect(genDiff('_fixtures_/file1.yml', '_fixtures_/file2.yml')).toEqual(outPut);
 });
