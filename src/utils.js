707d84970b04792406a290a5f913ee397eaff969
vlad221213 <vlad221213836@gmail.com>
2024-04-29 17:40:38 +0700

    add yml parse

--- src/utils.js
+++ src/utils.js
@@ -1,2 +1,15 @@
-const parse = (file) => JSON.parse(file);
+import { load } from 'js-yaml';
+import { readFileSync } from 'node:fs';
+
+const parse = (filePath) => {
+  const fileName = filePath.split('/').pop();
+  const formatName = fileName.slice(fileName.length - 5);
+  let result;
+  if (formatName === 'json') {
+    result = JSON.parse(readFileSync(filePath));
+  } else {
+    result = load(readFileSync(filePath));
+  }
+  return result;
+};
 export default parse;
