68fef019eb8c2eb88a08d60d30c42580fa9f1b63
vlad221213 <vlad221213836@gmail.com>
2024-05-06 18:43:11 +0700

    add utils functions

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 import { load } from 'js-yaml';
 import { readFileSync } from 'node:fs';
 
@@ -12,4 +13,80 @@ const parse = (filePath) => {
   }
   return result;
 };
-export default parse;
+
+const mkTree = (object) => {
+  const keysArray = Object.keys(object);
+  const result = keysArray.reduce((acc, key) => {
+    if (typeof object[key] === 'object' && object[key] !== null) {
+      const children = mkTree(object[key]);
+      const node = { name: key, children };
+      acc.push(node);
+    } else {
+      const node = { name: key, content: object[key] };
+      acc.push(node);
+    }
+    return acc;
+  }, []);
+  return result;
+};
+
+const sorting = (object) => {
+  const keysArray = Object.keys(object).sort((a, b) => {
+    if (a[2] < b[2]) {
+      return -1;
+    }
+    if (a[2] > b[2]) {
+      return 1;
+    }
+    return 0;
+  });
+  const result = keysArray.reduce((acc, key) => {
+    if (typeof object[key] === 'object' && object[key] !== null) {
+      acc[key] = sorting(object[key]);
+    } else {
+      acc[key] = object[key];
+    }
+    return acc;
+  }, {});
+  return result;
+};
+
+const spaceFormat = (object) => {
+  const keysArray = Object.keys(object);
+  const result = keysArray.reduce((acc, key) => {
+    if (typeof object[key] === 'object' && object[key] !== null && key[0] !== '+' && key[0] !== '-' && key[0] !== ' ') {
+      acc[`  ${key}`] = spaceFormat(object[key]);
+    } else if (typeof object[key] === 'object' && object[key] !== null && (key[0] === '+' || key[0] === '-' || key[0] === ' ')) {
+      acc[key] = spaceFormat(object[key]);
+    } else if (key[0] === '+' || key[0] === '-' || key[0] === ' ') {
+      acc[key] = object[key];
+    } else {
+      acc[`  ${key}`] = object[key];
+    }
+    return acc;
+  }, {});
+  return result;
+};
+
+const stylish = (object) => {
+  const nestingLevel = (structure, level = 1) => {
+    const tree = mkTree(structure);
+    let result = tree.reduce((acc, node) => {
+      if (Object.hasOwn(node, 'children')) {
+        const newLevel = level + 1;
+        acc += `\n${' '.repeat(level * 4 - 2)}${node.name}: ${nestingLevel(structure[node.name], newLevel)}`;
+      }
+      if (Object.hasOwn(node, 'content')) {
+        acc += `\n${' '.repeat(level * 4 - 2)}${node.name}: ${node.content}`;
+      }
+      return acc;
+    }, '{');
+    result += `\n${' '.repeat((level - 1) * 4)}}`;
+    return result;
+  };
+  return nestingLevel(object, 1);
+};
+
+export {
+  mkTree, sorting, spaceFormat, stylish, parse,
+};
