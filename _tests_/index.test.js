90bf04718351a79930ee4eba93a53043d82aca93
vlad221213 <vlad221213836@gmail.com>
2024-05-06 19:22:58 +0700

    refactoring

--- _tests_/index.test.js
+++ _tests_/index.test.js
@@ -7,12 +7,19 @@ import {
 
 const outPut = '{\n    common: {\n      + follow: false\n        setting1: Value 1\n      - setting2: 200\n      - setting3: true\n      + setting3: null\n        setting6: {\n            doge: {\n              - wow: \n              + wow: so much\n            }\n            key: value\n          + ops: vops\n        }\n      + setting4: blah blah\n      + setting5: {\n            key5: value5\n        }\n    }\n    group1: {\n      - baz: bas\n      + baz: bars\n        foo: bar\n      - nest: {\n            key: value\n        }\n      + nest: str\n    }\n  - group2: {\n        abc: 12345\n        deep: {\n            id: 45\n        }\n    }\n  + group3: {\n        deep: {\n            id: {\n                number: 45\n            }\n        }\n        fee: 100500\n    }\n}';
 const emptyObject = '{\n}';
+const genDiffTest = (filePath1, filePath2) => {
+  const file1 = parse(filePath1);
+  const file2 = parse(filePath2);
+  const sortingObject = sorting(spaceFormat(genDiff(file1, file2)));
+  return stylish(sortingObject);
+};
+
 test('JSONtest', () => {
-  expect(stylish(sorting(spaceFormat(genDiff(parse('_fixtures_/file1.json'), parse('_fixtures_/file2.json')))))).toEqual(outPut);
+  expect(genDiffTest('_fixtures_/file1.json', '_fixtures_/file2.json')).toEqual(outPut);
 });
 test('YMLtest', () => {
-  expect(stylish(sorting(spaceFormat(genDiff(parse('_fixtures_/file1.yml'), parse('_fixtures_/file2.yml')))))).toEqual(outPut);
+  expect(genDiffTest('_fixtures_/file1.yml', '_fixtures_/file2.yml')).toEqual(outPut);
 });
 test('emptyFileTest', () => {
-  expect(stylish(sorting(spaceFormat(genDiff(parse('_fixtures_/emptyFile.json'), parse('_fixtures_/emptyFile.json')))))).toEqual(emptyObject);
+  expect(genDiffTest('_fixtures_/emptyFile.json', '_fixtures_/emptyFile.json')).toEqual(emptyObject);
 });
