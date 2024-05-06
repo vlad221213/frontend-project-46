b37c1fcfd63cadcbff70a4c89f151c485d91b9b9
vlad221213 <vlad221213836@gmail.com>
2024-05-06 18:42:43 +0700

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,32 +1,46 @@
-/* eslint-disable no-param-reassign */
 /* eslint-disable import/extensions */
-import parse from './utils.js';
+/* eslint-disable quotes */
+import { mkTree } from "./utils.js";
 
-const genDiff = (filePath1, filePath2) => {
-  const file1 = parse(filePath1);
-  const file2 = parse(filePath2);
-  const file1Array = Object.entries(file1);
-  const itemsArray1 = file1Array.reduce((acc, [key, value]) => {
-    if (!Object.hasOwn(file2, key)) {
-      acc.push(`- ${key}: ${value}`);
+const genDiff = (file1, file2) => {
+  const tree1 = mkTree(file1);
+  const tree2 = mkTree(file2);
+  const diff1 = tree1.reduce((acc, node) => {
+    if (Object.hasOwn(node, 'children') && Object.hasOwn(file2, node.name) && typeof file2[node.name] === 'object') {
+      acc[`  ${node.name}`] = genDiff(file1[node.name], file2[node.name]);
     }
-    if (Object.hasOwn(file2, key) && file1[key] === file2[key]) {
-      acc.push(`  ${key}: ${value}`);
+    if (Object.hasOwn(node, 'children') && Object.hasOwn(file2, node.name) && typeof file2[node.name] !== 'object') {
+      acc[`- ${node.name}`] = file1[node.name];
+      acc[`+ ${node.name}`] = file2[node.name];
     }
-    if (Object.hasOwn(file2, key) && file1[key] !== file2[key]) {
-      acc.push(`- ${key}: ${file1[key]}`);
-      acc.push(`+ ${key}: ${file2[key]}`);
+    if (Object.hasOwn(node, 'children') && !Object.hasOwn(file2, node.name)) {
+      acc[`- ${node.name}`] = file1[node.name];
+    }
+    if (Object.hasOwn(node, 'content') && !Object.hasOwn(file2, node.name)) {
+      acc[`- ${node.name}`] = node.content;
+    }
+    if (Object.hasOwn(node, 'content') && file1[node.name] === file2[node.name]) {
+      acc[`  ${node.name}`] = node.content;
+    }
+    if (Object.hasOwn(node, 'content') && Object.hasOwn(file2, node.name) && file1[node.name] !== file2[node.name]) {
+      acc[`- ${node.name}`] = file1[node.name];
+      acc[`+ ${node.name}`] = file2[node.name];
     }
     return acc;
-  }, []);
-  const file2Array = Object.entries(file2);
-  const itemsArray2 = file2Array.reduce((acc, [key, value]) => {
-    if (!Object.hasOwn(file1, key)) {
-      acc.push(`+ ${key}: ${value}`);
+  }, {});
+  const diff2 = tree2.reduce((acc, node) => {
+    if (Object.hasOwn(node, 'children') && Object.hasOwn(file1, node.name) && typeof file1[node.name] === 'object') {
+      acc[`  ${node.name}`] = genDiff(file1[node.name], file2[node.name]);
+    }
+    if (Object.hasOwn(node, 'children') && !Object.hasOwn(file1, node.name)) {
+      acc[`+ ${node.name}`] = file2[node.name];
+    }
+    if (Object.hasOwn(node, 'content') && !Object.hasOwn(file1, node.name)) {
+      acc[`+ ${node.name}`] = node.content;
     }
     return acc;
-  }, []);
-  const unionItemsArray = [...itemsArray1, ...itemsArray2].sort((a, b) => a[2].localeCompare(b[2]));
-  return `{\n${unionItemsArray.join('\n')}\n}`;
+  }, diff1);
+  return diff2;
 };
+
 export default genDiff;
