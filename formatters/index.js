ae9f3509a08e1d744cd9cde96552134aea755992
vlad221213 <vlad221213836@gmail.com>
2024-05-25 16:04:24 +0700

    add json formater

--- formatters/index.js
+++ formatters/index.js
@@ -1,10 +1,14 @@
 /* eslint-disable import/extensions */
 import plain from './plain.js';
 import stylish from './stylish.js';
+import json from './json.js';
 
 const formaterSelection = (formaterName) => {
   if (formaterName === 'plain') {
     return plain;
+  }
+  if (formaterName === 'json') {
+    return json;
   }
   return stylish;
 };
