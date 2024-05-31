7af487689340cd644bdba0b33a5d6d40bfa2c21d
vlad221213 <vlad221213836@gmail.com>
2024-05-31 14:53:28 +0700

    correction

--- _tests_/index.test.js
+++ _tests_/index.test.js
@@ -2,21 +2,21 @@
 /* eslint-disable import/extensions */
 /* eslint-disable no-undef */
 import genDiff from '../index.js';
-import formaterSelection from '../formatters/index.js';
+import formatterSelection from '../formatters/index.js';
 import stylishOutput from '../_fixtures_/stylishOutput.js';
 import plainOutput from '../_fixtures_/plainOutput.js';
 
 const emptyObject = '{\n}';
 
 test('JSONtest', () => {
-  expect(genDiff('_fixtures_/file1.json', '_fixtures_/file2.json', formaterSelection('stylish'))).toEqual(stylishOutput);
+  expect(genDiff('_fixtures_/file1.json', '_fixtures_/file2.json', formatterSelection('stylish'))).toEqual(stylishOutput);
 });
 test('YMLtest', () => {
-  expect(genDiff('_fixtures_/file1.yml', '_fixtures_/file2.yml', formaterSelection('stylish'))).toEqual(stylishOutput);
+  expect(genDiff('_fixtures_/file1.yml', '_fixtures_/file2.yml', formatterSelection('stylish'))).toEqual(stylishOutput);
 });
 test('emptyFileTest', () => {
-  expect(genDiff('_fixtures_/emptyFile.json', '_fixtures_/emptyFile.json', formaterSelection('stylish'))).toEqual(emptyObject);
+  expect(genDiff('_fixtures_/emptyFile.json', '_fixtures_/emptyFile.json', formatterSelection('stylish'))).toEqual(emptyObject);
 });
 test('plainFormatTest', () => {
-  expect(genDiff('_fixtures_/file1.json', '_fixtures_/file2.json', formaterSelection('plain'))).toEqual(plainOutput);
+  expect(genDiff('_fixtures_/file1.json', '_fixtures_/file2.json', formatterSelection('plain'))).toEqual(plainOutput);
 });
