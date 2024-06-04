60a63c99fc97f736b81357623565ec5da1ba90ac
vlad221213 <vlad221213836@gmail.com>
2024-06-04 15:43:30 +0700

    add parsers

---
+++ src/parsers.js
@@ -0,0 +1,16 @@
+import { load } from 'js-yaml';
+import { readFileSync } from 'node:fs';
+
+const parse = (filePath) => {
+  const fileName = filePath.split('/').pop();
+  const formatName = fileName.slice(fileName.length - 4);
+  let result;
+  if (formatName === 'json') {
+    result = JSON.parse(readFileSync(filePath));
+  } else {
+    result = load(readFileSync(filePath));
+  }
+  return result;
+};
+
+export default parse;
